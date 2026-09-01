65a82d6dfdcea0a2f1463053904e07d424e7fe50
Victor Hugo Felipe Moreira <victor.hugo.felipe.moreira@gmail.com>
2026-08-31 22:48:25 -0300

    Consultas simples com query methods

--- src/main/java/com/example/demo/resources/PostResource.java
+++ src/main/java/com/example/demo/resources/PostResource.java
@@ -2,13 +2,18 @@ package com.example.demo.resources;
 
 
 
+
+import java.util.List;
+
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.domain.Post;
+import com.example.demo.resources.util.URL;
 import com.example.demo.services.PostService;
 
 
@@ -16,15 +21,22 @@ import com.example.demo.services.PostService;
 @RequestMapping(value = "/posts")
 public class PostResource {
 
-	private final PostService postService;
+	private final PostService service;
 
-	PostResource(PostService postService) {
-		this.postService = postService;
+	PostResource(PostService service) {
+		this.service = service;
 	}
 
 	@GetMapping(value = "/{id}") 
 	public ResponseEntity<Post> findById(@PathVariable String id) {
-		Post obj = postService.findById(id);
+		Post obj = service.findById(id);
 		return ResponseEntity.ok().body(obj);
+	}
+	
+	@GetMapping(value = "/titlesearch")
+	public ResponseEntity<List<Post>> findByTitle(@RequestParam(defaultValue = "") String text) {
+		text = URL.decodeParam(text);
+		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
 	}
 }
--- src/main/java/com/example/demo/services/PostService.java
+++ src/main/java/com/example/demo/services/PostService.java
@@ -1,5 +1,6 @@
 package com.example.demo.services;
 
+import java.util.List;
 import java.util.Optional;
 
 import org.springframework.stereotype.Service;
@@ -11,15 +12,19 @@ import com.example.demo.services.exception.ObjectNotFoundException;
 @Service
 public class PostService {
 
-	private final PostReporsitory postRepository;
+	private final PostReporsitory repo;
 
 	PostService(PostReporsitory repo) {
-		this.postRepository = repo;
+		this.repo = repo;
 	}
 	
 	public Post findById(String id) {
-		Optional<Post> post = postRepository.findById(id);
+		Optional<Post> post = repo.findById(id);
 		return post.orElseThrow(() -> new ObjectNotFoundException("Objeto não encontrado"));
+	}
+	
+	public List<Post> findByTitle(String text) {
+		return repo.findByTitleContainingIgnoreCase(text);
 	}
 	
 }
