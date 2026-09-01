f3bf2ee9ba096193c2d9316148f9fb0fff2685f1
Victor Hugo Felipe Moreira <victor.hugo.felipe.moreira@gmail.com>
2026-09-01 00:13:29 -0300

    Consultas de query method personalizada de full search inteligente

--- src/main/java/com/example/demo/resources/PostResource.java
+++ src/main/java/com/example/demo/resources/PostResource.java
@@ -3,6 +3,7 @@ package com.example.demo.resources;
 
 
 
+import java.util.Date;
 import java.util.List;
 
 import org.springframework.http.ResponseEntity;
@@ -37,6 +38,18 @@ public class PostResource {
 	public ResponseEntity<List<Post>> findByTitle(@RequestParam(defaultValue = "") String text) {
 		text = URL.decodeParam(text);
 		List<Post> list = service.findByTitle(text);
+		return ResponseEntity.ok().body(list);
+	}
+	
+	@GetMapping(value = "/fullsearch")
+	public ResponseEntity<List<Post>> fullSearch(
+			@RequestParam(defaultValue = "") String text,
+			@RequestParam(defaultValue = "") String minDate,
+			@RequestParam(defaultValue = "") String maxDate) {
+		text = URL.decodeParam(text);
+		Date min = URL.convertDate(minDate, new Date(0L));
+		Date max = URL.convertDate(maxDate, new Date());
+		List<Post> list = service.fullSearch(text, min, max);
 		return ResponseEntity.ok().body(list);
 	}
 }
